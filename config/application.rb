492cbeefe2a96c79ca2b5bf1b7a84a66cc694b48
Kryptyk Physh <kryptykphysh@kryptykphysh.xyz>
2020-05-11 20:58:44 +0100

    Use Docker locally, and Postgresql everywhere

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module CodChargen
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Use structure.sql instead of schema.rb
+    config.active_record.schema_format = :sql
   end
 end
